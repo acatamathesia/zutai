fe9ac7caff7054dbd35891c9c10f1de9c84de399
zhangyg <nkd2019@163.com>
2020-01-08 21:16:08 +0800

    修改text json中数据，

--- src/components/topo/control/ViewText.vue
+++ src/components/topo/control/ViewText.vue
@@ -5,12 +5,16 @@
         color: detail.style.foreColor,
         textAlign: textAlign,
         lineHeight: lineHeight + 'px',
-    }">
+    }" @click="simpleClick(detail)">
      {{detail.style.text}}
 </div>
 </template>
 
 <script>
+/**
+ * by zhangyg 2020-01-08
+ * 这里绑定的是输入框的数据
+ */
 import BaseView from './View';
 
 export default {
@@ -23,6 +27,22 @@ export default {
         return {
            
         }
+    },
+    methods: {
+         /**
+          * 实现对于点击内容的展示
+          * "dataBind": {
+                    "sn": "",
+                    "title": "",
+                    "biz": "",
+                    "queryParam": {},  这里绑定的是查询条件
+                    "dataid": "" 这里是我自己增加的字段用于获取后台的实时数据，保存的是数据点的ID
+                },
+          * @param detail
+          */
+         simpleClick(detail) {
+              console.log(detail)
+         }
     }
 }
 </script>
